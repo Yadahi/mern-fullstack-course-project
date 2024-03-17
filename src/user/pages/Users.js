dfdc1552ce9a0db604861a2f3eb464047072770f
Viliam <vatvil@gmail.com>
2024-03-17 13:28:35 +0100

    adding user component

--- src/user/pages/Users.js
+++ src/user/pages/Users.js
@@ -1,6 +1,16 @@
 import React from "react";
+import UserList from "../components/UsersList";
+
+const USERS = [
+  {
+    id: "u1",
+    name: "John",
+    image: "https://randomuser.me/api/portraits/men/75.jpg",
+    places: 3,
+  },
+];
 const Users = () => {
-  return <h2>Users works</h2>;
+  return <UserList items={USERS} />;
 };
 
 export default Users;
