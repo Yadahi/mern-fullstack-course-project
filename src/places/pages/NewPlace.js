3d7d6c4ee7bf84aa0f35688798c0be0c36cbd290
Viliam <vatvil@gmail.com>
2024-04-01 21:33:01 +0200

    custom input component

--- src/places/pages/NewPlace.js
+++ src/places/pages/NewPlace.js
@@ -1,8 +1,14 @@
 import React from "react";
+import Input from "../../shared/components/FormElements/Input";
+
 import "./NewPlace.css";
 
 const NewPlace = () => {
-  return <h2>New Place</h2>;
+  return (
+    <form className="place-form">
+      <Input element="input" type="text" id="title" label="Title" />
+    </form>
+  );
 };
 
 export default NewPlace;
