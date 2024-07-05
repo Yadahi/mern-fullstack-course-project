b967e1635487781625b224ee3f9f242e131ed293
Viliam <vatvil@gmail.com>
2024-07-05 20:59:29 +0200

    token expiration date

--- src/App.js
+++ src/App.js
@@ -17,11 +17,21 @@ const App = () => {
   const [token, setToken] = useState(false);
   const [userId, setUserId] = useState(false);
 
-  const login = useCallback((uid, token) => {
+  const login = useCallback((uid, token, expirationDate) => {
     setToken(token);
-    localStorage.setItem("userData", JSON.stringify({ userId: uid, token }));
     setUserId(uid);
+    const tokenExpirationDate =
+      expirationDate || new Date(new Date().getTime() + 1000 * 60 * 60);
+    localStorage.setItem(
+      "userData",
+      JSON.stringify({
+        userId: uid,
+        token,
+        expiration: tokenExpirationDate.toISOString(),
+      })
+    );
   }, []);
+
   const logout = useCallback(() => {
     setToken(null);
     setUserId(null);
@@ -32,10 +42,14 @@ const App = () => {
     const storedData = JSON.parse(localStorage.getItem("userData"));
     if (
       storedData &&
-      storedData.token
-      // new Date(storedData.expiration) > new Date()
+      storedData.token &&
+      new Date(storedData.expiration) > new Date()
     ) {
-      login(storedData.userId, storedData.token);
+      login(
+        storedData.userId,
+        storedData.token,
+        new Date(storedData.expiration)
+      );
     }
   }, [login]);
 
